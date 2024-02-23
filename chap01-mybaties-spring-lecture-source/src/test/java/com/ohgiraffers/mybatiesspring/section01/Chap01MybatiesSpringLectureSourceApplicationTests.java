a9d6fcbc27ace669b015c8da6e3ec0503411e8f4
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-02-23 17:57:03 +0900

    Spring에서 mybatis 라이브러리 추가하여 사용하기
    Spring에서 mybatis 라이브러리 추가하여 사용하기

---
+++ chap01-mybaties-spring-lecture-source/src/test/java/com/ohgiraffers/mybatiesspring/section01/Chap01MybatiesSpringLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.mybatiesspring.section01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01MybatiesSpringLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
